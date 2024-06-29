5b451b699bf2013e6851910be8db2c1b666d5d5e
Jay-Jay the JetPlane <jaythestarwarrior@gmail.com>
2024-06-29 04:12:22 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,104 +1,60 @@
-require "active_support/core_ext/integer/time"
+Otwarchive::Application.configure do
+# Settings specified here will take precedence over those in config/environment.rb
 
-Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+config.hosts = ArchiveConfig.PERMITTED_HOSTS
 
-  config.hosts = ArchiveConfig.PERMITTED_HOSTS
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+config.eager_load = true
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
+# Full error reports are disabled and caching is turned on
+config.consider_all_requests_local = true
+config.action_controller.perform_caching = true
+config.action_mailer.perform_caching = true
 
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+# Specifies the header that your server uses for sending files
+# config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+config.whiny_nils = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+config.logger = Logger.new(STDOUT)
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+# For nginx:
+# config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
+# If you have no front-end server that supports something like X-Sendfile,
+# just comment this out and Rails will serve the files
 
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
-  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
+# Disable IP spoofing protection
+config.action_dispatch.ip_spoofing_check = false
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+# See everything in the log (default is now :debug)
+# config.log_level = :debug
+config.log_level = :debug
 
-  # Use a different cache store in production
-  config.cache_store = :mem_cache_store, ArchiveConfig.MEMCACHED_SERVERS,
-                       { namespace: "ao3-v#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}",
-                         compress: true, pool_size: 10 }
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
 
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+# Use a different cache store in production
+config.cache_store = :mem_cache_store, ArchiveConfig.MEMCACHED_SERVERS,
+{ namespace: "ao3-v2", compress: true, pool_size: 10 }
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+# Disable Rails's static asset server
+# In production, Apache or nginx will already do this
+config.serve_static_files = true
 
-  # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII).
-  # config.log_level = :info
-  config.log_level = :error
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host = "http://assets.example.com"
 
-  # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+# Enable threaded mode
+# config.threadsafe!
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "otwarchive_production"
+# Send deprecation notices to registered listeners
+config.active_support.deprecation = :notify
 
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
-  # Don't log any deprecations.
-  config.active_support.report_deprecations = false
-
-  config.middleware.use Rack::Attack
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new($stdout)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
-  # paperclip config
-  Paperclip::Attachment.default_options[:storage] = :s3
-  Paperclip::Attachment.default_options[:s3_credentials] = { s3_region: ENV["S3_REGION"],
-                                                             bucket: ENV["S3_BUCKET"],
-                                                             access_key_id: ENV["S3_ACCESS_KEY_ID"],
-                                                             secret_access_key: ENV["S3_SECRET_ACCESS_KEY"] }
+config.middleware.use Rack::Attack
 end
